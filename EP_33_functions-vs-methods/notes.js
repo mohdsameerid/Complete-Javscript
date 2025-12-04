e39cf96361f910d7c4ad0aa05fcaac7161c35024
Mohd Samir Idrishi <mohdsamir413@gmail.com>
2025-12-04 21:37:14 +0530

    ep 33 added.

---
+++ EP_33_functions-vs-methods/notes.js
@@ -0,0 +1,13 @@
+
+
+function abc() {               // function 
+    console.log("ABC");
+}
+
+
+const user = {
+    name: "Samir",
+    a: function abc() {           // method 
+        console.log("ABC");
+    }
+}
