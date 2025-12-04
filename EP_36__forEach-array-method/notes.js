a59898925e8f5ce3e7fb3646b1c6d1d8e46322a0
Mohd Samir Idrishi <mohdsamir413@gmail.com>
2025-12-04 22:29:43 +0530

    ep 36 forEach added .

---
+++ EP_36__forEach-array-method/notes.js
@@ -0,0 +1,16 @@
+
+
+const fruits = ['banana', 'apple', 'peach']
+
+
+// fruits.forEach(function(fruit) {
+//     console.log(fruit);
+// })
+
+// banana
+// apple
+// peach
+
+
+
+// Note -> It does not return any value. 
