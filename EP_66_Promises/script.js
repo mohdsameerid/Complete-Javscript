7f5225aefde4de3d371c2d6028ad0f4c9270f66c
Mohd Samir Idrishi <mohdsamir413@gmail.com>
2025-12-13 12:06:27 +0530

    Promise with buttons

--- EP_66_Promises/script.js
+++ EP_66_Promises/script.js
@@ -1,14 +1,37 @@
 
-const p = new Promise((resolve, reject) => {
-    resolve('This promise is resolved');
-    // reject('This promise is rejected');
+// const p = new Promise((resolve, reject) => {
+//     resolve('This promise is resolved');
+//     // reject('This promise is rejected');
+// });
+
+// // console.log(p);
+
+
+// p.then((data) => {
+//     console.log('Inside then ', data);
+// }).catch((error) => {
+//     console.log('Inside catch ', error);
+// });
+
+
+
+
+
+///////////// Promise with buttons /////////////
+const resolveBtn = document.querySelector('#resolve-btn');
+const rejectBtn = document.querySelector('#reject-btn');
+
+const promise = new Promise((resolve, reject) => {
+    resolveBtn.addEventListener('click', () => {
+        resolve('Promise is resolved successfully');
+    });
+    rejectBtn.addEventListener('click', () => {
+        reject('Promise is rejected');
+    });
 });
 
-// console.log(p);
-
-
-p.then((data) => {
-    console.log('Inside then ', data);
+promise.then((data) => {
+    console.log(data);
 }).catch((error) => {
-    console.log('Inside catch ', error);
+    console.log(error);
 });
