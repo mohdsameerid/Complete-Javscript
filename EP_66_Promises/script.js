4a7ccd34900986d234742dcec16f3e420e6030db
Mohd Samir Idrishi <mohdsamir413@gmail.com>
2025-12-13 12:14:58 +0530

    Macrotask and Microtask differences

--- EP_66_Promises/script.js
+++ EP_66_Promises/script.js
@@ -18,17 +18,17 @@
 
 
 ///////////// Promise with buttons /////////////
-const resolveBtn = document.querySelector('#resolve-btn');
-const rejectBtn = document.querySelector('#reject-btn');
+// const resolveBtn = document.querySelector('#resolve-btn');
+// const rejectBtn = document.querySelector('#reject-btn');
 
-const promise = new Promise((resolve, reject) => {
-    resolveBtn.addEventListener('click', () => {
-        resolve('Promise is resolved successfully');
-    });
-    rejectBtn.addEventListener('click', () => {
-        reject('Promise is rejected');
-    });
-});
+// const promise = new Promise((resolve, reject) => {
+//     resolveBtn.addEventListener('click', () => {
+//         resolve('Promise is resolved successfully');
+//     });
+//     rejectBtn.addEventListener('click', () => {
+//         reject('Promise is rejected');
+//     });
+// });
 
 // promise.then((data) => {
 //     console.log(data);
@@ -38,20 +38,46 @@ const promise = new Promise((resolve, reject) => {
 
 
 //// Chaining of Promises ////
-promise
-    .then((data) => {
-        console.log(data);
-        return 'Data from first then block';
-    })
-    .then((data) => {
-        console.log(data);
-        return 'Data from second then block';
-    })
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+// promise
+//     .then((data) => {
+//         console.log(data);
+//         return 'Data from first then block';
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         return 'Data from second then block';
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
 
 //  Note -> This grow as Vertically not Horizontally like CallBack hell.   
+
+
+
+//// Event Loop - Macrotask vs Microtask ////
+// These two are examples of Macrotask and Microtask in JavaScript Event Loop
+// First microtasks are executed then macrotasks.
+
+setTimeout(() => {                                    // Macrotask
+    console.log('This is from setTimeout');
+}, 0)
+
+
+const myPromise = new Promise((resolve, reject) => {  // Microtask
+    console.log('Promise is created');
+    resolve('Promise is resolved');
+});
+
+
+myPromise.then((data) => {                            // Microtask
+    console.log('Inside then ', data);
+});
+
+
+// output: // Promise is created
+//         // Inside then  Promise is resolved
+//         // This is from setTimeout
