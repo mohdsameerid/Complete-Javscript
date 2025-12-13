eaffc78ef0610ea4a647b35daaee8e681cb467a3
Mohd Samir Idrishi <mohdsamir413@gmail.com>
2025-12-13 23:20:27 +0530

    ep 70 try catch added

---
+++ EP_70_try-catch/script.js
@@ -0,0 +1,34 @@
+
+
+// Def -> 
+
+
+const user = {
+    name: 'Samir',
+    age: 24
+}
+
+// console.log(user.details.name); // This will throw an error because 'details' is undefined
+
+// To handle such errors gracefully, we can use try-catch blocks
+
+try {
+    console.log(user.details.name);
+}
+catch (error) {
+    console.log('An error occurred:', error.message);
+}
+
+
+
+// try catch in async-await
+
+async function fetchUserData() {
+    try {
+        const response = await fetch('https://dummyjson.com/users/1');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log('Error fetching user data:', error.message);
+    }
+}
