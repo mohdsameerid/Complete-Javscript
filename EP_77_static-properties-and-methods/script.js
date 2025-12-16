b13ca1dcfdd5b755e966e57cc114ca8b03078f2d
Mohd Samir Idrishi <mohdsamir413@gmail.com>
2025-12-16 17:15:54 +0530

    static keyword in JS

---
+++ EP_77_static-properties-and-methods/script.js
@@ -0,0 +1,32 @@
+class CreateUser {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  }
+
+  static run = 'running'
+
+  static {
+    console.log(this);
+    this.hi = 'hello'
+    this.getFullName = function () {
+      return this.firstName + ' ' + this.lastName
+    }
+  }
+
+  getBirthYear() {
+    return new Date().getFullYear() - this.age
+  }
+
+}
+
+const user1 = new CreateUser('Aman', 'Mishra', 32)
+const user2 = new CreateUser('Anurag', 'Singh', 72)
+
+
+
+// static property only Accessible using Class . only returned created object :
+// user1 : not possible
+
+// CreateUser : CreateUser.run // possible
